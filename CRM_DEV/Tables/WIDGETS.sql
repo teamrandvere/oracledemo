f7e59bdc71d55385717df3ecf32092d7e69dd3f1
unknown <redgate@WIN2016>
2018-02-08 21:11:37 +0000

    made changes

--- CRM_DEV/Tables/WIDGETS.sql
+++ CRM_DEV/Tables/WIDGETS.sql
@@ -2,5 +2,6 @@ CREATE TABLE crm_dev.widgets (
   recordid NUMBER(*,0) NOT NULL,
   description VARCHAR2(50 BYTE),
   sku VARCHAR2(20 BYTE),
+  "NAME" VARCHAR2(20 BYTE),
   CONSTRAINT pk_widgets PRIMARY KEY (recordid)
 );
